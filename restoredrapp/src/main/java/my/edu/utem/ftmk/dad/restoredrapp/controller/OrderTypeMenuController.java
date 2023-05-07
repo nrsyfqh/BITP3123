b65d6c4530cc968e0e680f73604720743a882dee
nrsyfqh <nursyafiqahh132@gmail.com>
2023-05-07 23:30:16 +0800

    latest

--- restoredrapp/src/main/java/my/edu/utem/ftmk/dad/restoredrapp/controller/OrderTypeMenuController.java
+++ restoredrapp/src/main/java/my/edu/utem/ftmk/dad/restoredrapp/controller/OrderTypeMenuController.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- restoredrapp/src/main/java/my/edu/utem/ftmk/dad/restoredrapp/controller/ProductTypeMenuController.java
+++ restoredrapp/src/main/java/my/edu/utem/ftmk/dad/restoredrapp/controller/ProductTypeMenuController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
-import my.edu.utem.ftmk.dad.restoredrapp.model.OrderType;
+
 import my.edu.utem.ftmk.dad.restoredrapp.model.ProductType;
 
 @Controller
